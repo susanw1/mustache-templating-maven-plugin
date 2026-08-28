205c386f184d2665d499889bfd135fb201fd2d53
susanw1 <susanwitts1@gmail.com>
2026-08-28 11:52:15 +0100

    [#33] Ensure test uses non platform-specific line endings

--- mustache-templating-maven-plugin/src/it/nested-output-path/verify.groovy
+++ mustache-templating-maven-plugin/src/it/nested-output-path/verify.groovy
@@ -1,4 +1,4 @@
 def output = new File(basedir, 'target/generated-text/nested/example.txt')
 
 assert output.isFile()
-assert output.getText('UTF-8') == 'Nested: valid\n'
+assert output.getText('UTF-8') == "Nested: valid${System.lineSeparator()}"
